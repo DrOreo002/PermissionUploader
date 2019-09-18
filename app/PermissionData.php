691f656d5526044cf42e33087a1abffdf5524ac0
DrOreo002 Laptop <bagusanomsurya1@yahoo.com>
2019-09-18 08:28:05 +0700

    Update
    Updated ide-helper

--- app/PermissionData.php
+++ app/PermissionData.php
@@ -15,6 +15,16 @@ use Eloquent;
  * @property string $created_at
  * @property string $updated_at
  * @property bool $verified
+ * @property int $id
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\PermissionData newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\PermissionData newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\PermissionData query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\PermissionData whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\PermissionData whereFileName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\PermissionData whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\PermissionData whereSubmittedBy($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\PermissionData whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\PermissionData whereVerified($value)
  */
 class PermissionData extends Model
 {
